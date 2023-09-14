99ee915c1c0391526940dc2068ea48a00ab263fa
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-09-15 04:25:02 +0800

    install dependencies and main implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,54 @@
+import { useState } from 'react';
+import reviews from './data';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [id, setId] = useState(0);
+
+  const nextItem = () => {
+    const next = (id + 1) % reviews.length;
+    setId(next);
+  };
+
+  const prevItem = () => {
+    const prev = (id + reviews.length - 1) % reviews.length;
+    setId(prev);
+  };
+
+  return (
+    <main>
+      {reviews
+        .filter((_, index) => index === id)
+        .map((newReview) => {
+          const { id, image, job, name, text } = newReview;
+          return (
+            <article className="review" key={id}>
+              <div className="img-container">
+                <img src={image} alt={name} className="person-img" />
+                <span className="quote-icon"></span>
+              </div>
+              <h4 className="author">{name}</h4>
+              <p className="job">{job}</p>
+              <p className="info">{text}</p>
+              <div className="btn-container">
+                <button className="prev-btn" onClick={prevItem}>
+                  prev
+                </button>
+                <button className="next-btn" onClick={nextItem}>
+                  next
+                </button>
+              </div>
+              <button
+                className="btn btn-hipster"
+                onClick={() =>
+                  setId(Math.floor(Math.random() * reviews.length))
+                }
+              >
+                surprise me
+              </button>
+            </article>
+          );
+        })}
+    </main>
+  );
 };
 export default App;
